bf34637ef3f6153b68aa5074330a257435d50d51
colbisaurusrex <c.barthelmess1@gmail.com>
2017-03-27 15:41:20 -0700

    add routes as express middleware

--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
-const axios = require('axios');
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const routes = require('./routes');
 
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, '..', 'build')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use('/api', routes);
 const port = process.env.PORT || 8080;
 
 app.listen(port, () => console.log(`Listening on ${port}`));
