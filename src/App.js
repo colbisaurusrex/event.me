e542dd53aa88e6f2f881d882d785d281efd20547
colbisaurusrex <c.barthelmess1@gmail.com>
2017-03-27 14:33:19 -0700

    define client routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { HashRouter as Router, Route } from 'react-router-dom';  
+
 import './App.css';
+import Landing from './Landing.jsx';  
+import About from './About.jsx';  
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Router>  
+        <div className="App">
+          <Route exact path="/" component={Landing}/>
+          <Route path="/about"  component={About}/>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </Router>
     );
   }
 }
 
 export default App;
+
