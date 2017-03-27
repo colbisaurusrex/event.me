3452ca160c53905f812109208fa2ea1084ac2740
colbisaurusrex <c.barthelmess1@gmail.com>
2017-03-27 14:52:44 -0700

    move app to root folder

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { HashRouter as Router, Route } from 'react-router-dom';
+
+import './styles/App.css';
+import Landing from './components/Landing.jsx';
+import About from './components/About.jsx';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Route exact path="/" component={Landing} />
+          <Route path="/about" component={About} />
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
+
