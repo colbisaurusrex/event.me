1ce4d5824dde514be296f9e08f1e5a4941b90437
colbisaurusrex <c.barthelmess1@gmail.com>
2017-03-28 10:20:36 -0700

    add about component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -20,6 +20,8 @@ const About = () => (
             I am using Node.js/Express to serve the static assets. However, if the traffic were to get heavy, using a more efficient server, like Ngnix, is likely better. This would leave the Node sever to handle API requests. At any rate, with heavy traffic things change, but this is a small, low traffic app so I made this choice.
         </p>
       <h4>Axios</h4>
+      <h4>Redux</h4>
+      <h4>Redux Thunk</h4>
       <p>Using raw Node.js to manage the asyncronous calls to the STRV API certainly gives more control and abstracts less. But I chose to use the Axios library to manage this because it is promised based, is reliable and a breeze to use.</p>
       <h4>React-Create-App</h4>
       <p>I used the react-create-app created over at Facebook because I didn't want to reinvent the     wheel. It is a great tool to have at your disposal, especially during the prototype phase.</p>
