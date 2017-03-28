67515a766f95b98dbea1d4d51a09eeb58f445162
colbisaurusrex <c.barthelmess1@gmail.com>
2017-03-28 10:16:30 -0700

    remove conditional to add middleware to store

--- src/store.js
+++ src/store.js
@@ -3,14 +3,5 @@ import { createLogger } from 'redux-logger';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/index';
 
-let middleware;
-
-if (location.href.indexOf('://localhost')) {
-  middleware = applyMiddleware(createLogger(), thunk);
-} else {
-  middleware = applyMiddleware(thunk);
-}
-
-
-export default createStore(rootReducer, middleware);
+export default createStore(rootReducer, applyMiddleware(thunk, createLogger()));
 
