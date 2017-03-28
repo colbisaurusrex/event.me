5e65b4898368ce1b1652891f5be9d11cb97ca4af
colbisaurusrex <c.barthelmess1@gmail.com>
2017-03-28 10:18:01 -0700

    combine user and event reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 import user from './userReducer';
+import event from './eventReducer';
 
 const appReducer = combineReducers({
   user,
+  event,
 });
 export default appReducer;
