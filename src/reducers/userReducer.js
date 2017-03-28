9dd6e0f487925d446946fe5800bf74548c4ede44
colbisaurusrex <c.barthelmess1@gmail.com>
2017-03-27 22:06:45 -0700

    create user sign in reducer

---
+++ src/reducers/userReducer.js
@@ -0,0 +1,16 @@
+import { hashHistory } from 'react-router';
+
+export default function reducer(state, action) {
+  const newState = Object.assign({}, state);
+  switch (action.type) {
+    case 'USER SIGNIN SUCCESSFUL': {
+      window.localStorage.setItem('token', action.payload.token);
+      hashHistory.push('/about');
+      break;
+    }
+    case 'USER SIGNIN UNSUCCESSFUL': {
+      console.log('YOU MAY NOT PASS GO!');
+    }
+  }
+  return newState;
+}
