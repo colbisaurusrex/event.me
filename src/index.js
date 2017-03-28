29539cd81bc4a91e1275a281c13b7ee5db4b55c3
colbisaurusrex <c.barthelmess1@gmail.com>
2017-03-27 22:05:07 -0700

    wrap root component in Provider, pass store as prop

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
 import App from './App';
 import './styles/index.css';
+import Store from './store';
+
 
 ReactDOM.render(
-  <App />,
+  <Provider store={Store}>
+    <App />
+  </Provider>,
   document.getElementById('mount'),
 );
