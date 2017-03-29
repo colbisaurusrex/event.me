fab7a9d4748bfbe0c578197f3c03b01c97714c6f
colbisaurusrex <c.barthelmess1@gmail.com>
2017-03-29 12:32:19 -0700

    define attendEvent and flakeEvent actions

--- src/actions/eventActions.js
+++ src/actions/eventActions.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export function fetchEvents() {
+  console.log('fetchEvents fired');
   return dispatch => axios.get('api/events')
     .then((response) => {
       dispatch({ type: 'EVENT FETCH SUCCESSFUL', payload: response.data });
@@ -10,12 +11,43 @@ export function fetchEvents() {
     });
 }
 
-export function attendEvent() {
-  return dispatch => axios.post('api/events')
+export function attendEvent(eventid) {
+  const headers = {
+    token: window.localStorage.getItem('token'),
+  };
+  return dispatch => axios.post(`api/events?event_id=${eventid}`, {}, { headers })
   .then((response) => {
     dispatch({ type: 'ATTEND EVENT SUCCESSFUL', payload: response.data });
   })
   .catch((err) => {
     dispatch({ type: 'ATTEND EVENT UNSUCCESSFUL', payload: 'Could not attend event' });
+  });
+}
+
+export function flakeEvent(eventid) {
+  const data = {
+    token: window.localStorage.getItem('token'),
+  };
+  console.log(data);
+  console.log(eventid);
+  return dispatch => axios.delete(`api/events?event_id=${eventid}`, { data })
+  .then((response) => {
+    dispatch({ type: 'FLAKE EVENT SUCCESSFUL', payload: response.data });
+  })
+  .catch((err) => {
+    dispatch({ type: 'FLAKE EVENT UNSUCCESSFUL', payload: 'Could not attend event' });
+  });
+}
+
+export function updateEvent(eventid) {
+  const data = {
+    token: window.localStorage.getItem('token'),
+  };
+  return dispatch => axios.patch(`api/events?event_id=${eventid}`, { data })
+  .then((response) => {
+    dispatch({ type: 'UPDATE EVENT SUCCESSFUL', payload: response.data });
+  })
+  .catch((err) => {
+    dispatch({ type: 'UPDATE EVENT UNSUCCESSFUL', payload: 'Could not update event' });
   });
 }
