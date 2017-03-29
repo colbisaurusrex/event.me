7cc99c173d3982295fb623ae2c339f644edb344e
colbisaurusrex <c.barthelmess1@gmail.com>
2017-03-28 21:56:06 -0700

    create function to render a component for each event

---
+++ src/helpers/eventHelpers.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import moment from 'moment';
+import Event from '../components/Event.jsx';
+
+
+const renderEvents = (events, handler) => events.map(event =>
+  <Event
+    title={event.title}
+    description={event.description}
+    date={moment(event.startsAt).format('MMMM Do YYYY, h:mm a')}
+    owner={event.owner.firstName}
+    attendees={event.attendees.length}
+    capacity={event.capacity}
+    attendEvent={handler}
+  />,
+);
+
+module.exports = {
+  renderEvents,
+};
