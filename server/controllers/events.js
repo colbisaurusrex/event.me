323fd1009bc2e11e15d9617b3cd4d92d841d24e9
colbisaurusrex <c.barthelmess1@gmail.com>
2017-03-29 12:32:04 -0700

    define attendEvent and flakeEvent controllers

--- server/controllers/events.js
+++ server/controllers/events.js
@@ -8,5 +8,38 @@ module.exports = {
     })
     .catch(err => console.log(err));
   },
+  attendEvent: (req, res) => {
+    const token = req.get('token');
+    const headers = {
+      Authorization: token,
+    };
+    console.log(token);
+    console.log(req.query.event_id);
+    axios.post(`https://testproject-api.strv.com/events/${req.query.event_id}/attendees/me/`, {}, { headers })
+    .then((data) => {
+      res.json(data.data);
+    })
+    .catch(err => console.log(err));
+  },
+  flakeEvent: (req, res) => {
+    const headers = {
+      Authorization: req.body.token,
+    };
+    axios({ method: 'delete', url: `https://testproject-api.strv.com/events/${req.query.event_id}/attendees/me/`, headers })
+    .then((data) => {
+      res.json(data.data);
+    })
+    .catch(err => console.log(err));
+  },
+  updateEvent: (req, res) => {
+    const headers = {
+      Autorization: req.body.tokn,
+    };
+    axios.patch(`https://testproject-api.strv.com/events/${req.query.event_id}`)
+    .then((data) => {
+      console.log(data);
+    })
+    .catch(err => console.log(err));
+  },
 }
 ;
