c2bcf3e4ddc2f2e809c2136282867fc2b0b544cd
colbisaurusrex <c.barthelmess1@gmail.com>
2017-03-29 12:33:38 -0700

    define attend event and flake event reducers

--- src/reducers/eventReducer.js
+++ src/reducers/eventReducer.js
@@ -2,6 +2,7 @@ export default function reducer(state, action) {
   const newState = Object.assign({}, state);
   switch (action.type) {
     case 'EVENT FETCH SUCCESSFUL': {
+      console.log('fetch events SUCCESSFUL');
       newState.events = action.payload;
       break;
     }
@@ -9,10 +10,22 @@ export default function reducer(state, action) {
       console.log('UH OH, WE GOTS NO EVENTS');
     }
     case 'ATTEND EVENT SUCCESSFUL': {
-      newState.events = action.payload;
+      console.log('now attending event');
     }
     case 'ATTEND EVENT UNSUCCESSFUL': {
       console.log('UH OH, COULD NOT ATTEND THE EVENT');
+    }
+    case 'FLAKE EVENT SUCCESSFUL': {
+       console.log('You flaked!');
+     }
+    case 'FLAKE EVENT UNSUCCESSFUL': {
+      console.log('UH OH, you cannot flake');
+    }
+    case 'UPDATE EVENT SUCCESSFUL': {
+      console.log('update event successful, change action ');
+    }
+    case 'ATTEND EVENT UNSUCCESSFUL': {
+      console.log('UH OH, COULD NOT UPDATE THE EVENT');
     }
   }
   return newState;
