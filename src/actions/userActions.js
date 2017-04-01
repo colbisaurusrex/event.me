20b4c09a8035fecf2e8ae026c3b1c0f4354ddf6c
colbisaurusrex <c.barthelmess1@gmail.com>
2017-03-31 23:34:56 -0700

    define user signup and signout actions

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,11 +1,26 @@
 import axios from 'axios';
 
 export function signin(credentials) {
-  return dispatch => axios.post('api/', credentials)
+  return dispatch => axios.post('api/sigin', credentials)
     .then((response) => {
       dispatch({ type: 'USER SIGNIN SUCCESSFUL', payload: response.data });
     })
     .catch((err) => {
-      dispatch({ type: 'EVENT FETCH UNSUCCESSFUL', payload: '' });
+      dispatch({ type: 'USER SIGNIN UNSUCCESSFUL', payload: err });
     });
 }
+
+export function signup(details) {
+  return dispatch => axios.post('api/signup', details)
+    .then((response) => {
+      dispatch({ type: 'USER SIGNUP SUCCESSFUL', payload: response.data });
+    })
+    .catch((err) => {
+      dispatch({ type: 'USER SIGNUP UNSUCCESSFUL', payload: err });
+    });
+}
+
+export function signout(details) {
+  return { type: 'USER SIGN OUT', payload: 'signout' };
+}
+
