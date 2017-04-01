a815a6af3969111d5d4227f5b44c02448f267522
colbisaurusrex <c.barthelmess1@gmail.com>
2017-03-31 23:41:17 -0700

    add handlers, add create event component and nav buttons

--- src/components/EventDashBoard.jsx
+++ src/components/EventDashBoard.jsx
@@ -1,13 +1,39 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { renderEvents } from '../helpers/eventHelpers';
+import { hashHistory } from 'react-router';
+import { renderEvents, produceAttendeeList } from '../helpers/eventHelpers';
+import { signout } from '../actions/userActions';
+import CreateEvent from './CreateEvent.jsx';
+import fetching from '../assets/hatch_navigate.svg';
 
-const EventDashboard = props => (
-  <div className="eventcontainer">
-    {renderEvents(props.events)}
-  </div>
+const EventDashboard = (props) => {
+  const navigateToProfile = (e) => {
+    e.preventDefault();
+    hashHistory.push('/profile');
+  };
+  const signUserOut = (e) => {
+    e.preventDefault();
+    props.dispatch(signout())
+    .then(() => {
+      hashHistory.push('/');
+    })
+    .catch(err => console.log(err));
+  };
+  return (
+    <div>
+      {props.isFetching ? <img src={fetching} className="Loading App" alt="loading" /> :
+      <div className="eventcontainer">
+        <div onClick={navigateToProfile}>Your Profile</div>
+        <div onClick={signUserOut}>Sign Out</div>
+        <CreateEvent />
+        {renderEvents(props.events, produceAttendeeList)}
+      </div>
+      }
+    </div>
   );
+};
 
 export default connect(state => ({
   events: state.event.events,
+  isFetching: state.render.isFetching,
 }))(EventDashboard);
