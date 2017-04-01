16fd8b9ec17461521008071a8f2e93c55b10d634
colbisaurusrex <c.barthelmess1@gmail.com>
2017-03-31 23:27:25 -0700

    define updateEvent, deleteEvent, createEvent controllers

--- server/controllers/events.js
+++ server/controllers/events.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 
 module.exports = {
+
   getAll: (req, res) => {
     axios.get('https://testproject-api.strv.com/events')
     .then((data) => {
@@ -8,22 +9,23 @@ module.exports = {
     })
     .catch(err => console.log(err));
   },
+
   attendEvent: (req, res) => {
     const token = req.get('token');
     const headers = {
       Authorization: token,
-    };
-    console.log(token);
-    console.log(req.query.event_id);
+    }:
     axios.post(`https://testproject-api.strv.com/events/${req.query.event_id}/attendees/me/`, {}, { headers })
     .then((data) => {
+      console.log('attending');
       res.json(data.data);
     })
     .catch(err => console.log(err));
   },
+
   flakeEvent: (req, res) => {
     const headers = {
-      Authorization: req.body.token,
+      Authorization: req.get('token'),
     };
     axios({ method: 'delete', url: `https://testproject-api.strv.com/events/${req.query.event_id}/attendees/me/`, headers })
     .then((data) => {
@@ -31,15 +33,43 @@ module.exports = {
     })
     .catch(err => console.log(err));
   },
+
   updateEvent: (req, res) => {
     const headers = {
-      Autorization: req.body.tokn,
+      Authorization: req.get('token'),
     };
-    axios.patch(`https://testproject-api.strv.com/events/${req.query.event_id}`)
+    const data = {
+      title: req.body.title,
+      description: req.body.description,
+      capacity: req.body.capacity,
+      startsAt: req.body.startsAt,
+    };
+    axios({ method: 'patch', url: `https://testproject-api.strv.com/events/${req.query.event_id}`, data, headers })
     .then((data) => {
-      console.log(data);
+      res.json(data.data);
     })
     .catch(err => console.log(err));
   },
-}
-;
+
+  deleteEvent: (req, res) => {
+    const headers = {
+      Authorization: req.get('token'),
+    };
+    axios.delete(`https://testproject-api.strv.com/events/${req.query.event_id}`, { headers })
+    .then((data) => {
+      res.json(data.data);
+    })
+    .catch(err => console.log(err));
+  },
+
+  createEvent: (req, res) => {
+    const headers = {
+      Authorization: req.get('token'),
+    };
+    axios.post('https://testproject-api.strv.com/events', req.body, { headers })
+    .then((data) => {
+      res.json(data.data);
+    })
+    .catch(err => console.log(err));
+  },
+};
