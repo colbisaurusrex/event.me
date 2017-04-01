24851703d1de2c9d9eeae2770695b889c3c0571e
colbisaurusrex <c.barthelmess1@gmail.com>
2017-03-31 23:39:16 -0700

    add profile route, remove dashboard route

--- src/App.js
+++ src/App.js
@@ -2,20 +2,20 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Router, Route, hashHistory } from 'react-router';
 
-import './styles/App.css';
+import './styles/index.css';
 import Landing from './components/Landing.jsx';
-import Dashboard from './components/Dashboard.jsx';
 import EventDashboard from './components/EventDashBoard.jsx';
 import About from './components/About.jsx';
+import Profile from './components/Profile.jsx';
 
 class App extends Component {
   render() {
     return (
       <Router history={hashHistory}>
         <Route exact path="/" component={Landing} />
-        <Route path="/dashboard" component={Dashboard} />
         <Route path="/eventdashboard" component={EventDashboard} />
         <Route path="/about" component={About} />
+        <Route path="/profile" component={Profile} />
       </Router>
     );
   }
