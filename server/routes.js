80395127ba1eabe31e654ae5706c6ee996ac6bbe
colbisaurusrex <c.barthelmess1@gmail.com>
2017-03-31 23:03:11 -0700

    define additional user and event routes

--- server/routes.js
+++ server/routes.js
@@ -2,11 +2,13 @@ const routes = require('express').Router();
 const users = require('./controllers/users.js');
 const events = require('./controllers/events.js');
 
-routes.post('/', users.signin);
-
+routes.post('/sigin', users.signin);
+routes.post('/signup', users.signup);
 routes.get('/events', events.getAll);
-routes.post('/events/', events.attendEvent);
+routes.post('/events', events.attendEvent);
 routes.delete('/events', events.flakeEvent);
 routes.patch('/events', events.updateEvent);
+routes.delete('/singleevent', events.deleteEvent);
+routes.post('/createevent', events.createEvent);
 
 module.exports = routes;
