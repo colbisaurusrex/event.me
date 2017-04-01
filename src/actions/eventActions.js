7d03bf9e565ba17fd85bc2dc0de5cf691de4d0b7
colbisaurusrex <c.barthelmess1@gmail.com>
2017-03-31 23:30:11 -0700

    define deleteEvent, createEvent actions

--- src/actions/eventActions.js
+++ src/actions/eventActions.js
@@ -1,20 +1,22 @@
 import axios from 'axios';
 
+const headers = {
+  token: window.localStorage.getItem('token'),
+};
+
 export function fetchEvents() {
-  console.log('fetchEvents fired');
   return dispatch => axios.get('api/events')
     .then((response) => {
       dispatch({ type: 'EVENT FETCH SUCCESSFUL', payload: response.data });
+      dispatch({ type: 'IS FETCHING IS FALSE', payload: false });
     })
     .catch((err) => {
       dispatch({ type: 'EVENT FETCH UNSUCCESSFUL', payload: 'Could not get events' });
     });
 }
 
+
 export function attendEvent(eventid) {
-  const headers = {
-    token: window.localStorage.getItem('token'),
-  };
   return dispatch => axios.post(`api/events?event_id=${eventid}`, {}, { headers })
   .then((response) => {
     dispatch({ type: 'ATTEND EVENT SUCCESSFUL', payload: response.data });
@@ -25,12 +27,7 @@ export function attendEvent(eventid) {
 }
 
 export function flakeEvent(eventid) {
-  const data = {
-    token: window.localStorage.getItem('token'),
-  };
-  console.log(data);
-  console.log(eventid);
-  return dispatch => axios.delete(`api/events?event_id=${eventid}`, { data })
+  return dispatch => axios({ method: 'delete', url: `api/events?event_id=${eventid}`, headers })
   .then((response) => {
     dispatch({ type: 'FLAKE EVENT SUCCESSFUL', payload: response.data });
   })
@@ -39,15 +36,34 @@ export function flakeEvent(eventid) {
   });
 }
 
-export function updateEvent(eventid) {
-  const data = {
-    token: window.localStorage.getItem('token'),
-  };
-  return dispatch => axios.patch(`api/events?event_id=${eventid}`, { data })
+export function editEvent(details) {
+  return dispatch => axios.patch(`api/events?event_id=${details.eventId}`, details, { headers })
   .then((response) => {
+    console.log('response', response);
     dispatch({ type: 'UPDATE EVENT SUCCESSFUL', payload: response.data });
   })
   .catch((err) => {
     dispatch({ type: 'UPDATE EVENT UNSUCCESSFUL', payload: 'Could not update event' });
   });
 }
+
+export function deleteEvent(eventid) {
+  return dispatch => axios.delete(`api/singleevent?event_id=${eventid}`, { headers })
+  .then((response) => {
+    dispatch({ type: 'DELETE EVENT SUCCESSFUL', payload: response.data });
+  })
+  .catch((err) => {
+    dispatch({ type: 'DELETE EVENT UNSUCCESSFUL', payload: err });
+  });
+}
+
+export function createEvent(details) {
+  return dispatch => axios.post('api/createevent', details, { headers })
+  .then((response) => {
+    dispatch({ type: 'EVENT CREATION SUCCESSFUL', payload: response.data });
+  })
+  .catch((err) => {
+    dispatch({ type: 'EVENT CREATION SUCCESSFUL', payload: err });
+  });
+}
+
