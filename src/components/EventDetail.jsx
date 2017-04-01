13d2ef4a9b3bf15ec0760443270a54934d7ec2c3
colbisaurusrex <c.barthelmess1@gmail.com>
2017-03-31 23:40:25 -0700

    define event detail component, conditionally render elements based on event ownership

---
+++ src/components/EventDetail.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Line } from 'react-progressbar.js';
+import { connect } from 'react-redux';
+import { produceAttendeeList, renderEvents } from '../helpers/eventHelpers';
+import { fetchEvents, deleteEvent } from '../actions/eventActions';
+import { renderUpdate } from '../actions/renderActions';
+
+
+import Update from './Update.jsx';
+
+const EventDetail = (props) => {
+  const options = {
+    color: '#DE5745',
+    trailWidth: 1,
+    strokeWidth: 10,
+  };
+  const containerStyle = {
+    width: '100px',
+    height: '20px',
+  };
+
+  const showUpdate = (e) => {
+    e.preventDefault();
+    props.dispatch(renderUpdate(props.id));
+  };
+
+
+  return (
+    <div className=" bottom  hidden" >
+      <p>Attendance</p>
+      <div className="progressbar">
+        <Line progress={(props.attending / props.capacity)} containerStyle={containerStyle} initialAnimate options={options} containerClassName={'.progressbar'} />
+        <span>{props.attending}</span><span>/</span><span> {props.capacity}</span>
+      </div>
+      <div>{props.attendeeHelperFunction(props.attendeeList)}</div>
+      <p>Description:</p>
+      {props.description === '___' ? <p>The owner did not provide a description. So mysterious</p> : <p>{props.description}</p>}
+      {window.localStorage.getItem('userid') === props.owner_id ?
+        <div>
+          <Update
+            event_title={props.title}
+            event_description={props.description}
+            event_capacity={props.capacity}
+            event_id={props.event_id}
+          />
+        </div>
+        : null}
+    </div>
+  );
+};
+
+export default connect(state => ({
+  events: state.event.events,
+  componentID: state.render.componentID,
+}))(EventDetail);
