74ff591b476dc9e990a740b1f24869d87db9c0d4
colbisaurusrex <c.barthelmess1@gmail.com>
2017-03-31 23:42:36 -0700

    remove html and separate into sign in/sign out components

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,54 +1,22 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { signin } from '../actions/userActions';
-import { fetchEvents } from '../actions/eventActions';
 
+import SignIn from './SignIn.jsx';
+import SignUp from './SignUp.jsx';
+import Footer from './Footer.jsx';
 import logo from '../assets/hatch_attend.svg';
 
-
-const Landing = ({ dispatch }) => {
-  let emailInput = null;
-  let passwordInput = null;
-
-  const signInHandler = (e) => {
-    e.preventDefault();
-    dispatch(signin({
-      email: emailInput.value,
-      password: passwordInput.value,
-    }))
-    .then(() => {
-      dispatch(fetchEvents());
-    })
-    .catch(err => console.log(err));
-  };
-
-  return (
-    <div className="App">
-      <div>
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Events.me</h2>
-      </div>
-      <h1 className="experiment">Sign In</h1>
-      <form action="#" onSubmit={signInHandler}>
-        <div>
-          <input
-            required
-            type="email"
-            ref={(ref) => { emailInput = ref; }}
-          />
-        </div>
-        <div>
-          <input
-            required
-            type="password"
-            ref={(ref) => { passwordInput = ref; }}
-          />
-        </div>
-        <button type="submit">Sign In </button>
-      </form>
-      <h2>No Account? Sign up</h2>
+const Landing = props => (
+  <div className="App">
+    <div>
+      <img src={logo} className="App-logo" alt="logo" />
+      <h2>Events.me</h2>
     </div>
+    { props.signForm === 'in' ? <SignIn /> : <SignUp />}
+    <Footer />
+  </div>
   );
-};
 
-export default connect(null)(Landing);
+export default connect(state => ({
+  signForm: state.render.signForm,
+}))(Landing);
