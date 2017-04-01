836e432e71eb71c8873e2d36752dc7dbb5de28ab
colbisaurusrex <c.barthelmess1@gmail.com>
2017-03-31 23:28:28 -0700

    define signup controller

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -8,6 +8,21 @@ module.exports = {
       const resObject = {
         data: output.data,
         token: output.headers.authorization,
+        user_id: output.data.id,
+      };
+      res.json(resObject);
+    })
+    .catch(err => console.log(err));
+  },
+
+  signup: (req, res) => {
+    const { email, password, firstName, lastName } = req.body;
+    axios.post('https://testproject-api.strv.com/users', { email, password, firstName, lastName })
+    .then((output) => {
+      const resObject = {
+        data: output.data,
+        token: output.headers.authorization,
+        user_id: output.data.id,
       };
       res.json(resObject);
     })
