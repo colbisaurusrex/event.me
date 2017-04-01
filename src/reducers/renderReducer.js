6c3b6ee57f577a8ffbd75de4e9f90794d507fc68
colbisaurusrex <c.barthelmess1@gmail.com>
2017-03-31 23:43:36 -0700

    define render reducers

---
+++ src/reducers/renderReducer.js
@@ -0,0 +1,38 @@
+export default function reducer(state, action) {
+  const newState = Object.assign({}, state);
+  switch (action.type) {
+    case 'RENDER UPDATE': {
+      newState.update = true;
+      newState.componentID = action.payload;
+      break;
+    }
+    case 'HIDE UPDATE': {
+      newState.update = false;
+      newState.componentID = action.payload;
+      break;
+    }
+    case 'RENDER DETAILS': {
+      newState.details = true;
+      newState.componentID = action.payload;
+      break;
+    }
+    case 'RENDER SIGN UP FORM': {
+      newState.signForm = action.payload;
+      break;
+    }
+    case 'RENDER SIGN IN FORM': {
+      newState.signForm = action.payload;
+      break;
+    }
+    case 'IS FETCHING IS FALSE': {
+      newState.isFetching = action.payload;
+      break;
+    }
+    case 'IS FETCHING IS TRUE': {
+      newState.isFetching = action.payload;
+    }
+
+  }
+  // will need hide cases
+  return newState;
+}
